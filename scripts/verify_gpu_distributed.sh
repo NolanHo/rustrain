b5603bfd5a4355e4e91d10d89739d16efd6de77e
NolanHo <kujyo.eia.serias@gmail.com>
2026-06-23 09:00:37 +0800

    Verify unbounded DP Arrow cache training

--- scripts/verify_gpu_distributed.sh
+++ scripts/verify_gpu_distributed.sh
@@ -51,6 +51,9 @@ remote_run_2gpu env \
   bash scripts/verify_qwen_session_dp2_sft_arrow_worker.sh
 remote_run_2gpu env \
   RUSTRAIN_LAUNCH_TIMEOUT_SECS=900 \
+  bash scripts/verify_qwen_session_dp2_sft_arrow_unbounded_cache_worker.sh
+remote_run_2gpu env \
+  RUSTRAIN_LAUNCH_TIMEOUT_SECS=900 \
   bash scripts/verify_qwen_session_dp2_sft_arrow_index_cache_worker.sh
 remote_run_2gpu env \
   RUSTRAIN_LAUNCH_TIMEOUT_SECS=900 \
